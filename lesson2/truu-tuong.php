6555b0e6f292f87b9ff3efd4fa944b4bed289493
thienth32 <thienth32@gmail.com>
2020-04-21 07:28:08 +0700

    update

---
+++ lesson2/truu-tuong.php
@@ -0,0 +1,46 @@
+<?php
+// chuyển hóa đối tượng/sự vật/sự việc ở đề bài của bài toán hoặc các công cụ cần sử dụng 
+// quá trình làm ra sản phẩm (code) thành các câu lệnh (class)
+// xác định thuộc tính/hành động cần thiết của đối tượng thực tế => thuộc tính/phương thức
+class SinhVien{
+
+    var $hoten;
+    var $ngaysinh;
+    var $gioitinh;
+    var $tuoi;
+    var $sdtbo;
+    var $mssv;
+    var $cmnd;
+    var $avatar;
+    var $bangcap3;
+    // var $sdtanh;
+    // var $sdtchi;
+    var $diachi;
+    var $sdt;
+    // var $tinhtranghonnhan;
+    // var $chieucao;
+    // var $cannang;
+    // var $benhditruyen;
+    // var $mautoc;
+    // var $benhtruyennhiem;
+
+    function an(){}
+    function ngu(){}
+    function dihoc(){}
+    function hoclai(){}
+    function dithi(){}
+    function lamquiz(){}
+    function lamlab(){}
+    function henho(){}
+    function totinh(){}
+    function dabong(){}
+    function game(){}
+    function trada(){}
+    function diemdanh(){}
+
+
+}
+
+
+
+?>
